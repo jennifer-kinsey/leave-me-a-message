a771fee07cc0bbff8b862db871b0d117152e81e2
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-09-04 18:06:21 -0700

    updates controller and views for users' messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,31 +1,35 @@
 class MessagesController < ApplicationController
   def index
-    @messages = Message.all
+    @user = User.find(params[:user_id])
+    @messages = @user.messages.all
   end
 
   def new
-    @message = Message.new
+    @user = User.find(params[:user_id])
+    @message = @user.messages.new
   end
 
   def create
-    @message = Message.new(message_params)
+    @user = User.find(params[:user_id])
+    @message = @user.messages.new(message_params)
     if @message.save
       flash[:notice] = "Message successfully added"
-      redirect_to messages_path
+      redirect_to user_messages_path
     else
       render :new
     end
   end
 
   def destroy
+    @user = User.find(params[:user_id])
     @message = Message.find(params[:id])
     @message.destroy
     flash[:warning] = "Message successfully deleted"
-    redirect_to messages_path
+    redirect_to user_messages_path
   end
 
 private
   def message_params
-    params.require(:message).permit(:body)
+    params.require(:message).permit(:body, :user_id)
   end
 end
