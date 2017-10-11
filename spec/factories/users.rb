788b93e3c6fa3e459024cd376493c0fd7552ea0d
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-10-10 21:51:51 -0700

    update factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,25 @@
 FactoryGirl.define do
   factory :user do
-    
+    username 'K'
+    email 'k@email.com'
+    password 'password'
+    password_confirmation 'password'
+    admin false
+    id 1
+  end
+
+  # factory :admin, class: User do
+  #   username 'admin'
+  #   email 'admin2@email.com'
+  #   password 'password'
+  #   password_confirmation 'password'
+  #   admin true
+  #   id 2
+  # end
+
+  factory :message do
+    body 'hey, bud!'
+    favorite true
+    id 1
   end
 end
