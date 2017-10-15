716c1848ab50409e54ed46573f4827cfda36397c
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-10-14 23:06:03 -0700

    add a banned bool to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171015054015) do
+ActiveRecord::Schema.define(version: 20171015055817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20171015054015) do
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.boolean "admin", default: false
+    t.boolean "banned", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
